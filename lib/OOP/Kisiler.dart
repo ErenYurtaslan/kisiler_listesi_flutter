328fdbfc40e8fb5be9090787bceb477f9cedf878
Eren Yurtaslan <eren51abdulkadir@gmail.com>
2024-05-29 22:09:01 +0300

    lib\OOP\Kisiler.dart is added.

---
+++ lib/OOP/Kisiler.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: non_constant_identifier_names, file_names
+
+class Kisiler{
+  int kisi_id;
+  String kisi_ad;
+  String kisi_tel;
+
+  Kisiler(this.kisi_id, this.kisi_ad, this.kisi_tel);
+}
